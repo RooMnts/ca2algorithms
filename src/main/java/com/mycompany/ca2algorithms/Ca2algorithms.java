8e4af364d0441d72223f20fa72391a554f8d918b
Rodrigo Montes <roomnts@gmail.com>
2026-05-14 19:27:39 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/ca2algorithms/Ca2algorithms.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ca2algorithms;
+
+/**
+ *
+ * @author RooMnts 
+ */
+public class Ca2algorithms {
+
+    
+}
