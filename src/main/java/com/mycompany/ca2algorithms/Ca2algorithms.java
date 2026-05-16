d2cacbf497ffc91779e1a26936c6ce5f4d51cc10
Rodrigo Montes <roomnts@gmail.com>
2026-05-16 15:35:24 +0100

    Added basic employee fields and values

--- src/main/java/com/mycompany/ca2algorithms/Ca2algorithms.java
+++ src/main/java/com/mycompany/ca2algorithms/Ca2algorithms.java
@@ -10,4 +10,30 @@ package com.mycompany.ca2algorithms;
  */
 public class Ca2algorithms {
 
+    private String name;
+    private String managerType;
+    private String department;
+
+    public Ca2algorithms(String name, String managerType, String department) {
+        this.name = name;
+        this.managerType = managerType;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getManagerType() {
+        return managerType;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return name + " | " + managerType + " | " + department;
+    }
 }
