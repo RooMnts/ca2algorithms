42d04349cd56da179412f87fb062276583d68b13
Rodrigo Montes <roomnts@gmail.com>
2026-05-17 19:00:56 +0100

    Update Sorting.java

--- src/main/java/com/mycompany/ca2algorithms/Sorting.java
+++ src/main/java/com/mycompany/ca2algorithms/Sorting.java
@@ -4,10 +4,39 @@
  */
 package com.mycompany.ca2algorithms;
 
+import java.util.List;
+
 /**
  *
  * @author RooMnts
  */
 public class Sorting {
     
+    public static void mergeSort(List<String> list) {
+        if (list.size() <= 1) return;
+
+        int mid = list.size() / 2;
+        List<String> left = list.subList(0, mid);
+        List<String> right = list.subList(mid, list.size());
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(list, left, right);
+    }
+
+    private static void merge(List<String> list, List<String> left, List<String> right) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.size() && j < right.size()) {
+            if (left.get(i).compareToIgnoreCase(right.get(j)) <= 0) {
+                list.set(k++, left.get(i++));
+            } else {
+                list.set(k++, right.get(j++));
+            }
+        }
+
+        while (i < left.size()) list.set(k++, left.get(i++));
+        while (j < right.size()) list.set(k++, right.get(j++));
+    }
 }
