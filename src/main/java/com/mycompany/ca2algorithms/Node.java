1248aba64d0c9cb73585ce1fbde0763bbdc6fa75
Rodrigo Montes <roomnts@gmail.com>
2026-05-18 18:31:01 +0100

    Create & update Node.java

---
+++ src/main/java/com/mycompany/ca2algorithms/Node.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ca2algorithms;
+
+/**
+ *
+ * @author RooMnts
+ */
+public class Node {
+    
+    Ca2algorithms data;
+    Node left;
+    Node right;
+
+    public Node(Ca2algorithms data) {
+        this.data = data;
+    }    
+}
